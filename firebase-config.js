ea52c7c55149f640424228cafbafbf056dec6347
Jessica Calderon <90400224+Jessicalderon0503@users.noreply.github.com>
2025-04-28 09:40:53 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAj7cJ_8an3Y24Ib0SJmCSVD2b_I0okNGA",
+  authDomain: "basededatospaginawebpoli.firebaseapp.com",
+  projectId: "basededatospaginawebpoli",
+  storageBucket: "basededatospaginawebpoli.firebasestorage.app",
+  messagingSenderId: "501727061653",
+  appId: "1:501727061653:web:ac62d6993ed81e22365d9c"
+};
