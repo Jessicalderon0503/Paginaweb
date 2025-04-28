3f9bb30714d80d6fb2bd2be793753d52827888b9
Jessica Calderon <90400224+Jessicalderon0503@users.noreply.github.com>
2025-04-28 09:45:03 -0500

    Update index.js

--- img/index.js
+++ img/index.js
@@ -1,1 +1,9 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+// Importa firebaseConfig.js
+import { firebaseConfig } from './firebase-config';
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
